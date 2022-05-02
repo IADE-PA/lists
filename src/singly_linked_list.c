0c1ea17f97f81853480bee2776c72c8fae266581
amgs <amgs>
2022-05-02 14:44:54 +0100

    Done.

--- src/singly_linked_list.c
+++ src/singly_linked_list.c
@@ -15,6 +15,17 @@ struct List_ {
     size_t size;
 };
 
+Node _create_node(void* element) {
+    Node node = malloc(sizeof(struct Node_));
+    node->next = NULL;
+    node->element = element;
+    return node;
+}
+
+void _destroy_node(Node node) {
+    free(node);
+}
+
 // Creates a new list.
 List list_create() {
     List list = malloc(sizeof(struct List_));
@@ -29,7 +40,7 @@ void list_destroy(List list) {
     Node node = list->head;
     while (node != NULL) {
         Node next = node->next;
-        free(node);
+        _destroy_node(node);
         node = next;
     }
     free(list);
@@ -37,28 +48,41 @@ void list_destroy(List list) {
 
 // Returns true iff the list contains no elements.
 bool list_is_empty(List list) {
-    return false;
+    return list->size == 0;
 }
 
 // Returns the number of elements in the list.
 size_t list_size(List list) {
-    return 0;
+    return list->size;
 }
 
 // Returns the first element of the list.
 void* list_get_first(List list) {
-    return NULL;
+    if (list_is_empty(list)) {
+        return NULL;
+    }
+    return list->head->element;
 }
 
 // Returns the last element of the list.
 void* list_get_last(List list) {
-    return NULL;
+    if (list_is_empty(list)) {
+        return NULL;
+    }
+    return list->tail->element;
 }
 
 // Returns the element at the specified position in the list.
 // Range of valid positions: 0, ..., size()-1.
 void* list_get(List list, size_t position) {
-    return NULL;
+    if (position >= list->size) {
+        return NULL;
+    }
+    Node node = list->head;
+    for (size_t i = 0; i < position; i++) {
+        node = node->next;
+    }
+    return node->element;
 }
 
 // Returns the position in the list of the
@@ -66,15 +90,37 @@ void* list_get(List list, size_t position) {
 // or -1 if the specified element does not
 // occur in the list.
 int list_find(List list, void* element) {
-    return 0;
+    Node node = list->head;
+    for (size_t i = 0; i < list->size; i++) {
+        if (node->element == element) {
+            return i;
+        }
+        node = node->next;
+    }
+    return -1;
 }
 
 // Inserts the specified element at the first position in the list.
 void list_insert_first(List list, void* element) {
+    Node node = _create_node(element);
+    node->next = list->head;
+    list->head = node;
+    if (list_is_empty(list)) {
+        list->tail = node;
+    }
+    list->size++;
 }
 
 // Inserts the specified element at the last position in the list.
 void list_insert_last(List list, void* element) {
+    Node node = _create_node(element);
+    if (list_is_empty(list)) {
+        list->head = node;
+    } else {
+        list->tail->next = node;
+    }
+    list->tail = node;
+    list->size++;
 }
 
 // Inserts the specified element at the specified position in the list.
@@ -82,24 +128,92 @@ void list_insert_last(List list, void* element) {
 // If the specified position is 0, insert corresponds to insertFirst.
 // If the specified position is size(), insert corresponds to insertLast.
 void list_insert(List list, void* element, size_t position) {
+    if (position > list->size) {
+        return;
+    }
+    if (position == 0) {
+        list_insert_first(list, element);
+    } else if (position == list->size) {
+        list_insert_last(list, element);
+    } else {
+        Node node = _create_node(element);
+        Node prev = list->head;
+        for (size_t i = 0; i < position - 1; i++) {
+            prev = prev->next;
+        }
+        node->next = prev->next;
+        prev->next = node;
+        list->size++;
+    }
 }
 
 // Removes and returns the element at the first position in the list.
 void* list_remove_first(List list) {
-    return NULL;
+    if (list_is_empty(list)) {
+        return NULL;
+    }
+    Node node = list->head;
+    list->head = node->next;
+    if (list->head == NULL) {
+        list->tail = NULL;
+    }
+    void* element = node->element;
+    _destroy_node(node);
+    list->size--;
+    return element;
 }
 
 // Removes and returns the element at the last position in the list.
 void* list_remove_last(List list) {
-    return NULL;
+    if (list_is_empty(list)) {
+        return NULL;
+    }
+    void* element = list->tail->element;
+    if (list_size(list) == 1) {
+        list_make_empty(list);
+    } else {
+        Node node = list->head;
+        while (node->next != list->tail) {
+            node = node->next;
+        }
+        list->tail = node;
+        list->size--;
+    }
+    return element;
 }
 
 // Removes and returns the element at the specified position in the list.
 // Range of valid positions: 0, ..., size()-1.
 void* list_remove(List list, size_t position) {
-    return NULL;
+    if (position >= list_size(list)) {
+        return NULL;
+    } else if (position == 0) {
+        return list_remove_first(list);
+    } else if (position == list_size(list) - 1) {
+        return list_remove_last(list);
+    }
+
+    Node prev = list->head;
+    for (size_t i = 0; i < position - 1; i++) {
+        prev = prev->next;
+    }
+    Node node = prev->next;
+    void* element = node->element;
+    prev->next = node->next;
+    _destroy_node(node);
+    list->size--;
+    return element;
 }
 
 // Removes all elements from the list.
 void list_make_empty(List list) {
+    Node node = list->head;
+    while (node != NULL) {
+        Node next = node->next;
+        _destroy_node(node);
+        node = next;
+    }
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
 }
